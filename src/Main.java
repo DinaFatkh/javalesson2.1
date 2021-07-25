18eb3327ed89a60dda369292798bd396604bf21e
Fatkhutdinova Dina <dinafatkh@gmail.com>
2021-07-25 17:59:13 +0300

    Second commit

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        int balance = 2000000000;
-        int transfer = 500000000;
+        int balance = 1_647_483_648;
+        int transfer = 500_000_000;
         int remainder = balance + transfer;
         System.out.println(remainder);
     }
