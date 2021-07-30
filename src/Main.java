482522ed483e259812cd1f32f948e4a9a86e89d0
Fatkhutdinova Dina <dinafatkh@gmail.com>
2021-07-30 21:20:18 +0300

    Chenges 30.7

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        int balance = 1_647_483_648;
-        int transfer = 500_000_000;
+        int balance = 258_483_648;
+        int transfer = 0;
         int remainder = balance + transfer;
         System.out.println(remainder);
     }
